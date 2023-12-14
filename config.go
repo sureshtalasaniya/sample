2dcfb6dacfc04b6ec168207cd98b4eb0335c403f
Suresh Talsaniya <suresh.talasaniya@gmail.com>
2023-12-14 23:50:49 +0530

    Create config.go

---
+++ config.go
@@ -0,0 +1,93 @@
+
+package amaze
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+)
+
+// Config - Configuration struct, designed to be
+type Config struct {
+	ApiUrl              string
+	ApiVersion          string
+	AccountSid          string
+	AuthToken           string
+	ResponseContentType string
+}
+
+// OptionString - Will return ENV value back based on provided name casted as string
+func OptionString(name, def string) string {
+	if res := os.Getenv(name); res != "" {
+		return string(res)
+	}
+
+	return def
+}
+
+// Validate - Will validate client configuration and return error if any issues are found
+func (c *Config) Validate() error {
+	if _, err := url.ParseRequestURI(c.ApiUrl); err != nil {
+		return fmt.Errorf("Make sure to provide valid Api URL. You've provided: %s. Error: %s", c.ApiUrl, err)
+	}
+
+	if c.ApiVersion != "v2" {
+		return fmt.Errorf("Make sure to provide valid api version. You've provided: %s.", c.ApiVersion)
+	}
+
+	if len(c.AccountSid) != 34 || !strings.HasPrefix(c.AccountSid, "AC") {
+		return fmt.Errorf("Make sure to provide valid AccountSid. You've provided: %s", c.AccountSid)
+	}
+
+	if len(c.AuthToken) != 32 {
+		return fmt.Errorf("Make sure to provide valid AuthToken. You've provided: %s", c.AuthToken)
+	}
+
+	if !StringInSlice(c.ResponseContentType, AvailableResponseContentTypes) {
+		return fmt.Errorf("Make sure to provide valid response content type. You've provided: %s. Available are: %v", c.AuthToken, AvailableResponseContentTypes)
+	}
+
+	return nil
+}
+
+// SetCredentials - Will set account sid and auth token to desired account
+func (c *Config) SetCredentials(accountSid, authToken string) error {
+	if len(accountSid) != 34 || !strings.HasPrefix(accountSid, "AC") {
+		return fmt.Errorf("Make sure to provide valid AccountSid. You've provided: %s", accountSid)
+	}
+
+	if len(authToken) != 32 {
+		return fmt.Errorf("Make sure to provide valid AuthToken. You've provided: %s", authToken)
+	}
+
+	c.AccountSid = accountSid
+	c.AuthToken = authToken
+	return nil
+}
+
+// NewConfig -
+func NewConfig() (*Config, error) {
+	cfg := &Config{
+		ApiUrl:              OptionString("AMAZE_CLOUD_API_URL", AmazeApiUrl),
+		ApiVersion:          OptionString("AMAZE_CLOUD_API_VERSION", AmazeApiVersion),
+		AccountSid:          OptionString("AMAZE_CLOUD_ACCOUNT_SID", ""),
+		AuthToken:           OptionString("AMAZE_CLOUD_AUTH_TOKEN", ""),
+		ResponseContentType: OptionString("AMAZE_CLOUD_RESPONSE_TYPE", DefaultResponseContentType),
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// NewCustomConfig -
+func NewCustomConfig(cfg *Config) (*Config, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
