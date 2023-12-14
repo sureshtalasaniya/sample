3f10373aec3b557b1282a84802d3f400b451b9e4
Suresh Talsaniya <suresh.talasaniya@gmail.com>
2023-12-14 23:41:40 +0530

    Create constant.go

---
+++ constant.go
@@ -0,0 +1,19 @@
+/*
+ * Define Default Values
+*/
+
+package amaze
+
+var (
+	// AvailableResponseContentTypes -
+	AvailableResponseContentTypes = []string{"xml", "json"}
+)
+
+const (
+	// AmazeApiUrl -
+	AmazeApiUrl = "https://api.amaze.com"
+	// AmazeApiVersion -
+	AmazeApiVersion = "v2"
+	// DefaultResponseContentType -
+	DefaultResponseContentType = "json"
+)
